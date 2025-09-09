aee0e8286293834daf444a45c520b0a604623160
FaizAmd22 <ahmadfaizhal8@gmail.com>
2025-09-09 17:48:29 +0700

    fix dploy

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,7 +1,5 @@
-"use client";
-
 import { Provider } from "@/components/ui/provider";
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  return <Provider>{children}</Provider>;
+  return <Provider value={{}}>{children}</Provider>;
 }
