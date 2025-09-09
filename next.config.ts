b59c0b485fa1ce9859e46d163953500af86fbc04
FaizAmd22 <ahmadfaizhal8@gmail.com>
2025-09-09 18:08:02 +0700

    fix deploy

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["drive.google.com"],
   },
-  experimental: {
-    optimizePackageImports: ["@chakra-ui/react"],
-  },
 };
 
 export default nextConfig;
